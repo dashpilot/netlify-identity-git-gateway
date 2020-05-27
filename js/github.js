03eff4107c36389e06693575a5b248163fb100bb
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-05-28 00:04:22 +0200

    test

--- js/github.js
+++ js/github.js
@@ -1,6 +1,8 @@
 function getData(mypath = '') {
 
-
+    let user = netlifyIdentity.currentUser()
+    console.log(user)
+    let token = user.token.access_token
 
     var url = "/.netlify/git/github/contents/" + mypath;
     var bearer = 'Bearer ' + token;
@@ -28,7 +30,7 @@ function saveData(mypath, data) {
 
     getData(mypath).then(function(curfile) {
 
-        let user = await netlifyIdentity.currentUser()
+        let user = netlifyIdentity.currentUser()
         let token = user.token.access_token
 
         let opts = {
